ead60fa82b2f94a25b13389b99d7a18d5b642bf4
Gian ricardo Berkenbrock <gian.ricardo@gmail.com>
2015-05-22 16:50:26 -0300

    Segunda versão de hoje.

---
+++ lista/ListaLab01/src/programa.cc
@@ -0,0 +1,24 @@
+/*
+ * programa.cc
+ *
+ *  Created on: May 22, 2015
+ *      Author: gian
+ */
+
+#include <iostream>
+#include "node.h"
+
+int main(int argc, char **argv) {
+    Node<int> no;
+    std::cout << "Listas" << std::endl;
+}
+
+//todo SE com ref no inicio
+//todo SE com ref no inicio e no fim
+//todo SE com ref para uma posição corrente e no inicio e fim
+//todo DE com ref no inicio
+//todo DE com ref no inicio e no fim
+//todo DE com ref para uma posição corrente e no inicio e fim
+//todo SE-Circular com ref para uma posição corrente e no inicio e fim
+//todo DE-Circular com ref para uma posição corrente e no inicio e fim
+
